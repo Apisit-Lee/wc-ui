a995c2adc4d867e7f63e2d83326727c8cdda62c3
lihexin <htxin.li@chelaile.net.cn>
2021-11-30 15:31:27 +0800

    add first component file: /components/portal.js

---
+++ components/portal.js
@@ -0,0 +1,40 @@
+export default class WcPortal extends HTMLElement {
+  static get observedAttributes() {
+    return ['portal'];
+  }
+  constructor() {
+    super();
+    const shadowRoot = this.attachShadow({ mode: 'open' });
+    shadowRoot.innerHTML = '<slot></slot>';
+  }
+  get portal() {
+      const portal = this.getAttribute('portal');
+      if (portal) {
+          return document.querySelector(portal)
+      }
+      return null;
+  }
+  set portal(portal) {
+    this.setAttribute(portal);
+  }
+  connectedCallback() {
+    if (this.portal && this.portal!==this.parentNode) {
+        this.portal.appendChild(this);
+    }
+  }
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (name === 'portal') {
+      if (oldValue) {
+        const oldPortal = document.quesySelector(oldValue);
+        oldPortal.removeChild(this);
+      }
+      if (this.portal && this.portal!==this.parentNode) {
+        this.portal.appendChild(this);
+      }
+    }
+  }
+}
+
+if (!customElements.get('wc-portal')) {
+    customElements.define('wc-portal', WcPortal);
+}
