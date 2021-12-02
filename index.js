c32a735d7b4da2702281b87242d17b44743b83bc
Apisit Lee <Apisit-Lee@users.noreply.github.com>
2021-12-02 18:30:29 +0800

    test import script

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import './components/portal.js';
-import './components/icon.js';
-import './components/icon-source.js';
-import './components/text.js';
+import 'https://raw.githubusercontent.com/Apisit-Lee/wc-ui/master/components/portal.js';
+import 'https://raw.githubusercontent.com/Apisit-Lee/wc-ui/master/components/icon.js';
+import 'https://raw.githubusercontent.com/Apisit-Lee/wc-ui/master/components/icon-source.js';
+import 'https://raw.githubusercontent.com/Apisit-Lee/wc-ui/master/components/text.js';
